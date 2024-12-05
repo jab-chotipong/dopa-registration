83c4a237c4aadd8025450bd1e2109958377808a4
jabchotipong <jab.chotipong@gmail.com>
2024-12-05 15:41:19 +0700

    fix: limited upload files

--- app/_components/InputFile.tsx
+++ app/_components/InputFile.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
+import { useEffect } from 'react'
 import {
   FieldError,
   FieldValues,
   RegisterOptions,
   useFormContext,
-} from "react-hook-form";
-import { GoPlusCircle } from "react-icons/go";
-import { IoMdClose } from "react-icons/io";
+} from 'react-hook-form'
+import { GoPlusCircle } from 'react-icons/go'
+import { IoMdClose } from 'react-icons/io'
 
 type InputFileProps = {
-  label: string;
-  name: string;
-  disabled?: boolean;
-  required?: boolean;
-  isImage?: boolean;
-  onClick?: () => void;
-  rule?: RegisterOptions<FieldValues, string> | undefined;
-};
+  label: string
+  name: string
+  disabled?: boolean
+  required?: boolean
+  isImage?: boolean
+  onClick?: () => void
+  rule?: RegisterOptions<FieldValues, string> | undefined
+}
 
 export function InputFile(props: InputFileProps) {
   const {
@@ -25,72 +25,72 @@ export function InputFile(props: InputFileProps) {
     setValue,
     setError,
     formState: { errors },
-  } = useFormContext();
+  } = useFormContext()
 
-  const file = watch(props.name);
+  const file = watch(props.name)
 
   const handleOnClick = () => {
-    props.disabled && props.onClick && props.onClick();
-  };
+    props.disabled && props.onClick && props.onClick()
+  }
 
   const handleFileError = (type: any) => {
     switch (type) {
-      case "required":
-        return "กรุณาเพิ่มไฟล์";
-      case "maximum":
-        return "ขนาดไฟล์เกิน 2 MB";
-      case "acceptedFormats":
-        return "กรุณาใส่รูปภาพให้ถูกต้อง";
+      case 'required':
+        return 'กรุณาเพิ่มไฟล์'
+      case 'maximum':
+        return 'ขนาดไฟล์เกิน 2 MB'
+      case 'acceptedFormats':
+        return 'กรุณาใส่รูปภาพให้ถูกต้อง'
       default:
-        return "";
+        return ''
     }
-  };
+  }
 
   return (
     <div
-      className="text-blue-800 w-full flex flex-col gap-4 border border-slate-300 p-4 rounded-lg relative"
+      className='text-blue-800 w-full flex flex-col gap-4 border border-slate-300 p-4 rounded-lg relative'
       onClick={handleOnClick}
     >
       <p>{props.label}</p>
       <label
         htmlFor={props.name}
-        className="flex gap-4 items-center w-full bg-slate-200 p-2 rounded-lg justify-center border border-dashed overflow-hidden whitespace-nowrap text-ellipsis border-blue-800 relative"
+        className='flex gap-4 items-center w-full bg-slate-200 p-2 rounded-lg justify-center border border-dashed overflow-hidden whitespace-nowrap text-ellipsis border-blue-800 relative'
       >
         {file && file[0] ? (
-          <p className="text-blue-800 text-ellipsis overflow-hidden">
+          <p className='text-blue-800 text-ellipsis overflow-hidden'>
             {file[0]?.name}
           </p>
         ) : (
           <>
-            <GoPlusCircle className="w-5 h-5" />
-            <p className="text-blue-800 text-ellipsis overflow-hidden">
+            <GoPlusCircle className='w-5 h-5' />
+            <p className='text-blue-800 text-ellipsis overflow-hidden'>
               เพิ่ม{props.label}
             </p>
           </>
         )}
-        {typeof file === "string" ? (
-          <p className="text-ellipsis overflow-hidden">{file}</p>
+        {typeof file === 'string' ? (
+          <p className='text-ellipsis overflow-hidden'>{file}</p>
         ) : (
           <>
             <input
               id={props.name}
               disabled={props.disabled}
-              type="file"
-              className="hidden"
+              type='file'
+              className='hidden'
               {...register(props.name, {
                 required: props.required,
                 validate: {
                   maximum: (v) => {
                     if (v?.length > 0)
-                      return v[0]?.size < 2097152 || "ขนาดไฟล์เกิน 2 MB";
+                      return v[0]?.size < 6291456 || 'ขนาดไฟล์เกิน 5 MB'
                   },
                   acceptedFormats: (files) => {
-                    if (file?.length > 0 && props.name == "image")
+                    if (file?.length > 0 && props.name == 'image')
                       return (
-                        ["image/jpeg", "image/png", "image/jpg"].includes(
+                        ['image/jpeg', 'image/png', 'image/jpg'].includes(
                           files[0]?.type
-                        ) || "กรุณาใส่รูปในนามสกุล jpeg, png"
-                      );
+                        ) || 'กรุณาใส่รูปในนามสกุล jpeg, png'
+                      )
                   },
                 },
               })}
@@ -99,18 +99,18 @@ export function InputFile(props: InputFileProps) {
         )}
       </label>
       {errors[props.name] && (
-        <p className="text-[12px] absolute top-[-16px] right-0 text-red-500">
+        <p className='text-[12px] absolute top-[-16px] right-0 text-red-500'>
           {handleFileError(errors[props.name]?.type)}
         </p>
       )}
       {file?.length > 0 && !props.disabled && (
         <IoMdClose
           onClick={() => {
-            setValue(props.name, null);
+            setValue(props.name, null)
           }}
-          className="absolute right-4 cursor-pointer"
+          className='absolute right-4 cursor-pointer'
         />
       )}
     </div>
-  );
+  )
 }
